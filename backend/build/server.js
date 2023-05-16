bdd80094d8474dce202b032faa2892d81a0c2f88
SafiullahRattar <safiullahrattar313@gmail.com>
2023-05-17 03:14:45 +0500

    chaneges

--- backend/build/server.js
+++ backend/build/server.js
@@ -72,5 +72,5 @@ app.use(errorMiddleware_1.errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
-    console.log('NODE_ENV: ', process.env.NODE_ENV);
+    console.log("NODE_ENV: ", process.env.NODE_ENV);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -87,5 +87,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
-  console.log('NODE_ENV: ', process.env.NODE_ENV);
+  console.log("NODE_ENV: ", process.env.NODE_ENV);
 });
